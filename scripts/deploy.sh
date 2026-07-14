9369604e0ec783c1fd94b984031bbcc307dbc8c3
adamko <adaled00@gmail.com>
2026-07-14 23:31:51 +0200

    cicd update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,12 @@ APP_DIR=/opt/momentkaph_be
 APP_NAME=BE
 NGINX_DEST=/etc/nginx/
 
+# ---- app: swap dist, keep previous for rollback, atomic replace ----
+rm -rf "$APP_DIR/dist.prev"
+[ -d "$APP_DIR/dist" ] && mv "$APP_DIR/dist" "$APP_DIR/dist.prev"
+mkdir -p "$APP_DIR/dist"
 staging=$(mktemp -d "$APP_DIR/.dist.new.XXXXXX")
 tar -xzf /tmp/BE.tar.gz -C "$staging"
+mv "$staging"/* "$APP_DIR/dist"
+rm -rf "$staging"
 
